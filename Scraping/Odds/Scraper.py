7bc50816d2f95ac74f547eee22fdd8a54b1d861c
hunt <hunt.b.graham@gmail.com>
2015-03-22 16:38:32 -0400

    Restructured project structure

---
+++ Scraping/Odds/Scraper.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+import requests
+from BeautifulSoup import BeautifulSoup
+import unicodedata
+from BettingLine import Team, BettingLine
+
+class Scraper():
+
+	def __init__(self):
+		t = Team.blankTeam()
+		self.teams = ['x', 'a', 'b']
+		#if 'xsdf' in self.teams:
+			#print 'found'
+		#else:
+			#print 'nope'
+		#self.teamList = [
+	
+	#teams 
+	url = 'http://sports.bovada.lv/sports-betting/nba-basketball-lines.jsp'
+	
+	def Run(self):
+		r = requests.get(self.url)
+		soup = BeautifulSoup(r.text)
+
+		divs = soup.findAll('div', 'event left even')
+		divs += soup.findAll('div', 'event left odd')
+		
+		self.Dump(divs, 'dump')
+
+		betting_lines = []
+		for i, div in enumerate(divs):
+			print '\nSection: ' + str(i)
+			betting_lines.append(self.ParseSection(div))
+			
+		print '\nCount:'
+		print len(divs)
+		
+	def ParseSection(self, section):
+		t = Team.blankTeam()
+	
+		self.Dump(section, 'indiv')
+		##
+
+		# Find spreads
+		spreads = section.findAll('a', 'lineOdd')
+		if not spreads:
+			try:
+				spreads = section.find('div', 'line-normal').find('span', 'disabled')
+			except Exception, e: #If no line-normal found then this bet is 'suspended'
+				print 'Parse Error: ' + str(e)
+				return
+		if not spreads or len(spreads) < 2:
+			print 'SPREADS NOT FOUND'
+			return None
+		else:
+			#print 'spread 1: ' + str(spreads[0])
+			#print 'spread 1: ' + str(spreads[1])
+			print 'spread 1: ' + spreads[0].text
+			print 'spread 1: ' + spreads[1].text
+		
+		# Find names
+		names = section.findAll('a', 'competitor left')
+		if not names:
+			names = section.findAll('span', 'left disabled')
+		if not names or len(names) < 2:
+			print 'NAMES NOT FOUND.'
+			return None
+		else:
+			print 'Team 1: ' + names[0].text
+			print 'Team 2: ' + names[1].text
+			
+		# Validate teams
+		#if not self.CheckTeam(spreads[0], spreads[1]): # TODO change this to return a tuple for team 1 and 2
+			#return None
+		
+	def CheckTeam(self, teamName):
+		retrun (teamName in self.teams)
+		
+	def CheckLine(line):
+		return True
+		
+	def LoadTeams(self):
+		sqlString = 'SELECT * FROM TEAMS'
+		return ['steelers', 'ravens', 'patriots', 'jets']
+		# dal
+		
+	def Dump(self, text, location):
+		open(location, 'w').write(str(text))
+		
+sraper = Scraper()
+sraper.Run()
---
+++ Scraping/SportData/ScheduleScraper.py
@@ -0,0 +1,77 @@
+import requests
+import urllib2
+import json
+import xml.etree.ElementTree as ET
+from lxml import objectify
+
+
+class ScheduleScraper():
+	'''
+	SELECT * 
+		FROM  `Team` 
+		WHERE CONCAT( Location,  ' ', Name ) =  'Boston Celtics'
+	'''
+	
+	sqlString = 'SELECT ID, Name, Location FROM Team'
+
+	url = 'http://api.sportsdatallc.org/nba-t3/games/2014/REG/schedule.json?api_key=3m8xndzddcvjc9wahux5wvye'
+	
+	def __init__(self):
+		self.LoadCaches()
+	
+	def LoadCaches(self):
+		## query all teams
+		## create dict by location + name string (id is content)
+		self.teamCache = {'San Antonio Spurs' : 1 }
+		pass
+	
+	def Dump(self, txt):
+		open('xmldump.txt', 'w').write(txt)
+	
+	def Run(self):
+		r = requests.get(self.url)
+		decoded = json.loads(r.text)
+
+		self.Dump(json.dumps(decoded, sort_keys=True, indent=4))
+		
+		game1 = decoded['games'][0]
+
+		# home team
+		homename = game1['home']['name']
+		homealias = game1['home']['alias']
+		homeid = game1['home']['id']
+		
+		# away team
+		awayname = game1['away']['name']
+		awayalias = game1['home']['alias']
+		awayid = game1['home']['id']
+		
+		home_id = self.CheckTeam(homename)
+		away_id = self.CheckTeam(awayname)
+		
+		if not home_id or not away_id:
+			print 'Invalid team'
+			return
+		
+		id = game1['id']
+		venue = game1['venue']['city'] + ' ' + game1['venue']['name']
+		date = game1['scheduled']
+		
+		print id
+		print venue
+		
+		print homename
+		print awayalias
+		print awayid
+		
+		print awayname
+		return True
+	
+	def CheckTeam(self, team):
+		if team in self.teamCache:
+			return self.teamCache[team]
+		else:
+			return None
+		
+sc = ScheduleScraper()
+sc.Run()
