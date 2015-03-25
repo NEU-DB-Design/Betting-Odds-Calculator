e8561aef224d285ac468cb690fc2135a534f49e6
h <h@h.com>
2015-03-25 01:49:26 +0000

    Got scraper working finally.

--- Scraping/Odds/Scraper.py
+++ Scraping/Odds/Scraper.py
@@ -67,12 +67,11 @@ class Scraper():
 		r = requests.get(self.url)
 		soup = BeautifulSoup(r.text)
 
-		all = []
 		lst = []
 		dt = None
 		
 		scheduleContainer = soup.find('div', id='event-schedule')
-		
+
 		for dv in scheduleContainer.findAll('div', recursive=False):
 			cnt = ' '.join(dv.attrs['class'])
 			# Set this to the date for all odds read until we find another of these
@@ -85,11 +84,10 @@ class Scraper():
 				if cnt == u'event left even' or cnt == u'event left odd':
 					section = self.ParseSection(dv, dt)
 				if not not section:
-					lst.append(section)
-		#for odd in lst:
-			#print str(odd.game_id)
-			#print str(odd.spread)
+					lst.append(section) # Add to DB here
+		print len(lst)
 	
+
 	def ParseDate(self, section):
 		# Find span with date
 		dateText = section.find('span').text
@@ -106,28 +104,63 @@ class Scraper():
 			month = self.months[trm[1]]
 
 		return datetime.date(year=int(trm[3]), month=month, day=int(trm[2]))
+	
+
+	def ParseSpread(self, spread):
+		spread = spread.strip()
+		spr = None
+		
+		for i, ch in enumerate(spread):
+			if ch == u'½':
+				spr = int(spread[0:i])
+				if spread[0] == u'-':
+					spr -= 0.5
+				else:
+					spr += 0.5
+				break
+			elif ch == u'(':
+				spr = int(spread[0:i])
+				break
+		if not spr:
+			spr = int(spread)
+
+		return spr
 
 		
 	def ParseSection(self, section, date):
 		# Find spreads
-		spreads = section.findAll('a', 'lineOdd')
+
+		#spreads = section.findAll('a', 'lineOdd')
+		spreads = section.find('a', 'lineOdd')
 		if not spreads:
+			#print 'not spreads'
 			try:
-				spreads = section.find('div', 'line-normal').find('span', 'disabled')
+				#spreads = section.find('div', 'line-normal').find('span', 'disabled')
+				spreads = section.find('div', 'line-normal').find('span')
+				#print 'SPREAD 1  ' +  spr 
 			except Exception, e: #If no line-normal found then this bet is 'suspended'
 				print 'Parse Error: ' + str(e)
 				return None
-		if not spreads or len(spreads) < 2:
-			#print 'SPREADS NOT FOUND'
+
+		if not spreads :
 			return None
-		#else:
-			#print 'spread 1: ' + spreads[0].text
-			#print 'spread 2: ' + spreads[1].text
+
+		ml1, ml2 = None, None
+		try:
+			conts = section.findAll('div', 'moneyline-line-normal')
+			ml1 = int(conts[0].text)
+			ml2 = int(conts[1].text)
+		except Exception, e:	
+			print 'MoneyLine Error:  ' + str(e)
+
+		if not not ml1 and not not ml2:
+			print 'ML 1: ' + str(ml1)
+			print 'ML 2: ' + str(ml2)
 		
-		spread1 = spreads[0].text
-		spr = spread1.split(' ')[0]
-		
-		spread2 = spreads[1].text
+
+		print 'spread 1: ' + spreads.text
+		ps = self.ParseSpread(spreads.text)
+		print 'parsed spread: ' + str(ps)
 
 		# Find names
 		names = section.findAll('a', 'competitor left')
@@ -137,37 +170,42 @@ class Scraper():
 			print 'NAMES NOT FOUND.'
 			return None
 		#else:
-		##print 'Team 1: ' + names[0].text
-		#print 'Team 2: ' + names[1].text
-		#print spr
-		#print 'Moneyline:  ' + spreads[1].text
+		print 'Team 1: ' + names[0].text
+		print 'Team 2: ' + names[1].text
+
 		t1 = names[0].text
 		t2 = names[1].text
 
-		#return t1, t2
-		
 		t1_id = self.CheckTeam(t1)
 		t2_id = self.CheckTeam(t2)
 		
 		if not t1_id or not t2_id:
-			print 'Not found :('
+			print 'Team not found :('
 			return None
 
 		#print 'FOUND!!!!!!'
-		#game_id = self.SearchGame(t1_id, t2_id, date)
 		game_id = self.FindGame(t1_id, t2_id, date)
-		#print 'GAME:  ' + str(game)
+
 		# spread and ML are null fo rnow
 		if not game_id:
-			#print t1_id
-			#print t2_id
-			#print date
-			print "NONE"
 			return None
-		else:
-			return BettingLine(game_id, '', spr, '')
+		self.SaveLine(game_id, ml1, ml2, ps)
 
-		#return t1_id, t2_id, spread1, spread2
+	def SaveLine(self, game_id, m1, m2, spread):
+		print 'Attempting save...'
+		print 'spread to check: ' + str(spread)
+		print m1
+		print m2
+		print game_id
+		if not spread:
+			print 'spread is null indeed!!'
+		try:
+			cnx, cursor = self.GetCursor()
+			args = (game_id, m1, m2, spread)
+			cursor.callproc('createNewOdd', args=args)
+			cnx.commit()
+		except Exception, e:
+			print 'Save Error:  ' + str(e)
 			
 	def LoadCaches(self):
 		self.team_ID_Cache = {}
@@ -180,25 +218,21 @@ class Scraper():
 			if not key in self.team_ID_Cache:
 				self.team_ID_Cache[key] = _id
 
-		print 'Team id cache loaded.'
+		print '\nTeam id cache loaded.\n'
 
 	def LoadGameCache(self):
+		# Init cache
 		self.gameCache = {}
+
+		# Call proc
 		cnx, cursor = self.GetCursor()
-		#x = (0,)
-		#res2 = cursor.callproc('simpleproc3', args=x)
-		#print 'proc res:  ' + str(res2)
-		#return
-		#cursor.execute(self.str1)
 		cursor.callproc('GetGameKeys')
-		#print 'count: ' + str(len(result_args))
-		#print 'res;  ' + str(result_args)
-		#for res in cursor.fetchall():
-			#print 'res:  ' + str(res)
-		#for _id, team1id, team2id, date in cursor.fetchall():
+		
+		# Create a key for each team
 		for _id, team1id, team2id, date in cursor.fetchall():
 			key = str(team1id) + '_' + str(team2id) + '_' + str(date)
 			key2 = str(team2id) + '_' + str(team1id) + '_' + str(date)
+			#print 'key to check: ' + key 
 			if not key in self.gameCache:
 				self.gameCache[key] = _id
 			if not key2 in self.gameCache:
@@ -206,11 +240,13 @@ class Scraper():
 
 	def FindGame(self, id1, id2, date):
 		key = str(id1) + '_' + str(id2) + '_' + str(date)
+		print 'key to check: ' + key 
 		if key in self.gameCache:
-			print 'found game!'
+			print 'found game!\n'
+			return self.gameCache[key]
 		else:
-			print 'no juice'
-		print key
+			print 'no juice\n'
+			return None
 
 	def SearchGame(self, id1, id2, date):
 		cnx, cursor = self.GetCursor()
@@ -238,4 +274,6 @@ class Scraper():
 		open(location, 'w').write(str(text))
 		
 sraper = Scraper()
+#print 'RES:  ' + str(sraper.ParseSpread(u'+10'))
 sraper.Run()
+
